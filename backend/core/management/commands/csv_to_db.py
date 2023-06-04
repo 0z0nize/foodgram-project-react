f0c50b151ef272cbd5150ee921974d0c2338ee93
Владислав Шкаровский <vladislav.shkarovskiy@yandex.ru>
2023-06-04 20:58:03 +0300

    data

--- backend/core/management/commands/csv_to_db.py
+++ backend/core/management/commands/csv_to_db.py
@@ -4,7 +4,7 @@ import os.path
 from colorama import Fore
 from django.conf import settings
 from django.core.management import BaseCommand
-from recipes.models import Ingredient
+from recipes.models import Ingredient, Tag
 
 
 class Command(BaseCommand):
@@ -14,6 +14,8 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options) -> None:
         """Создает объекты Tag."""
+        self.stdout.write(Fore.RED + 'Clear ingredients data')
+        Ingredient.objects.all().delete()
         self.stdout.write(Fore.BLUE + 'Trying to load ingredients data')
         file_path = os.path.join(settings.DATA_ROOT, 'ingredients.csv')
         with open(file_path, 'r', encoding='utf-8') as f:
@@ -24,4 +26,18 @@ class Command(BaseCommand):
                 )
             self.stdout.write(
                 Fore.GREEN + 'Ingredients data successfully uploaded'
+            )
+        self.stdout.write(Fore.RED + 'Clear tags data')
+        Tag.objects.all().delete()
+        self.stdout.write(Fore.BLUE + 'Trying to load ingredients data')
+        file_path = os.path.join(settings.DATA_ROOT, 'tags.csv')
+        with open(file_path, 'r', encoding='utf-8') as f:
+            reader = csv.reader(f)
+            for tag, color, slug in reader:
+                Tag.objects.get_or_create(
+                    name=tag, color=color,
+                    slug=slug
+                )
+            self.stdout.write(
+                Fore.GREEN + 'Tags data successfully uploaded'
             )
--- backend/foodgram/settings/components/folders.py
+++ backend/foodgram/settings/components/folders.py
@@ -8,4 +8,4 @@ STATIC_ROOT = os.path.join(BASE_DIR.parent.parent, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR.parent.parent, 'media')
 
-DATA_ROOT = os.path.join(BASE_DIR.parent.parent, 'data')
+DATA_ROOT = os.path.join(BASE_DIR.parent, 'data')
