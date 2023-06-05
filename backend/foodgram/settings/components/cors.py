9bd91566abdbcd5cadd6d62c68533c8429fcfbff
Владислав Шкаровский <vladislav.shkarovskiy@yandex.ru>
2023-06-05 14:35:48 +0300

    location

--- backend/foodgram/settings/components/cors.py
+++ backend/foodgram/settings/components/cors.py
@@ -2,7 +2,7 @@
 
 CORS_URLS_REGEX = r'^/api/.*$'
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',
+    'http://localhost:3000', 'http://62.84.124.211:3000'
 ]
 #     os.environ.get('CORS_ALLOWED'),
 # ]
