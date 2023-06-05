fd3e10a8d93be8ccd1dd8618122a1786db5e6217
Владислав Шкаровский <vladislav.shkarovskiy@yandex.ru>
2023-06-05 07:50:26 +0300

    allowed host

--- backend/foodgram/settings/components/common.py
+++ backend/foodgram/settings/components/common.py
@@ -11,9 +11,13 @@ load_dotenv(os.path.join(BASE_DIR.parent, 'infra/.env'), verbose=True)
 
 SECRET_KEY = os.environ.get('DJANGO_KEY', default='H3R3-Y0UR-S3CR3T-K3Y')
 
-DEBUG = True # os.environ.get('TEST_ENVIRONMENT', default=False) == 'True'
+DEBUG = True  # os.environ.get('TEST_ENVIRONMENT', default=False) == 'True'
 
-ALLOWED_HOSTS = ['62.84.124.211', 'localhost', '127.0.0.1',]
+ALLOWED_HOSTS = [
+    '62.84.124.211',
+    'localhost',
+    '127.0.0.1',
+]
 #     os.environ.get('ALLOWED_HOSTS', default='*'),
 # ]
 
--- backend/foodgram/settings/components/cors.py
+++ backend/foodgram/settings/components/cors.py
@@ -1,6 +1,8 @@
 # import os
 
 CORS_URLS_REGEX = r'^/api/.*$'
-CORS_ALLOWED_ORIGINS = ['http://localhost:3000',]
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+]
 #     os.environ.get('CORS_ALLOWED'),
 # ]
