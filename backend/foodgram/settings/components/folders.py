7077ba99bc8c6abeb449d50510c03852ef6935d1
Владислав Шкаровский <vladislav.shkarovskiy@yandex.ru>
2023-06-05 15:14:39 +0300

    folders

--- backend/foodgram/settings/components/folders.py
+++ backend/foodgram/settings/components/folders.py
@@ -3,9 +3,9 @@ import os
 from foodgram.settings.components.common import BASE_DIR
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR.parent.parent, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR.parent, 'static')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR.parent.parent, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR.parent, 'media')
 
 DATA_ROOT = os.path.join(BASE_DIR.parent, 'data')
