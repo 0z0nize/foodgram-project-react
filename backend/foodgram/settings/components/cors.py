95bf37377c58ea2593bc5cf2150ba699efe757e2
Владислав Шкаровский <vladislav.shkarovskiy@yandex.ru>
2023-06-07 22:47:31 +0300

    rollback

--- backend/foodgram/settings/components/cors.py
+++ backend/foodgram/settings/components/cors.py
@@ -1,4 +1,6 @@
 import os
 
 CORS_URLS_REGEX = r'^/api/.*$'
-CORS_ALLOWED_ORIGINS = os.environ.get('CORS_ALLOWED', default='*').split(' ')
+CORS_ALLOWED_ORIGINS = [
+    os.environ.get('CORS_ALLOWED'),
+]
